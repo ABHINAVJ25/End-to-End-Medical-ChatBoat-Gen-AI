8a24f776f8fac4bd2a2165a9ef837decf851540d
ABHINAVJ25 <abhinavjain25d@gmail.com>
2025-04-07 15:20:14 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # End-to-End-Medical-ChatBoat-Gen-AI
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author= 'ABHINAV JAIN',
+    author_email= "abhinavjain25d@gmail.com",
+    packages= find_packages(),
+    install_requires= []
+)
